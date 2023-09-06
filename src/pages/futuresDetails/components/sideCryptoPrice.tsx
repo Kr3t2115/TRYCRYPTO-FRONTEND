0d2ce2655990bddbbf3e6fa1f477f8ba36d7b378
Kr3t2115 <szymon.baranowicz@teb.edu.pl>
2023-09-06 22:20:36 +0200

    Zmiana modal

--- src/pages/futuresDetails/components/sideCryptoPrice.tsx
+++ src/pages/futuresDetails/components/sideCryptoPrice.tsx
@@ -8,92 +8,90 @@ import BuyCrypto from "../../../services/buyCrypto";
 import { Link } from "react-router-dom";
 
 interface TickerInterface {
-    e: string; // Event type
-    E: number; // Event time
-    s: string; // Symbol
-    ps: string; // Pair
-    p: string; // Price change
-    P: string; // Price change percent
-    w: string; // Weighted average price
-    c: string; // Last price
-    Q: string; // Last quantity
-    o: string; // Open price
-    h: string; // High price
-    l: string; // Low price
-    v: string; // Total traded volume
-    q: string; // Total traded base asset volume
-    O: number; // Statistics open time
-    C: number; // Statistics close time
-    F: number; // Last trade Id
-    n: number; // Total number of trades
+  e: string; // Event type
+  E: number; // Event time
+  s: string; // Symbol
+  ps: string; // Pair
+  p: string; // Price change
+  P: string; // Price change percent
+  w: string; // Weighted average price
+  c: string; // Last price
+  Q: string; // Last quantity
+  o: string; // Open price
+  h: string; // High price
+  l: string; // Low price
+  v: string; // Total traded volume
+  q: string; // Total traded base asset volume
+  O: number; // Statistics open time
+  C: number; // Statistics close time
+  F: number; // Last trade Id
+  n: number; // Total number of trades
 }
 
-  
-export default function SideCryptoPrice({symbol}: any) {
- 
+export default function SideCryptoPrice({ symbol }: any) {
   const { currentBalance, auth, setCurrentBalance }: any =
-  useContext(AuthContext);
-  
-    const [show, setShow] = useState<boolean>(false);
+    useContext(AuthContext);
 
-    const [data, setData] = useState<TickerInterface>({
-        e: "", // Event type
-        E: 0, // Event time
-        s: "", // Symbol
-        ps: "", // Pair
-        p: "", // Price change
-        P: "", // Price change percent
-        w: "", // Weighted average price
-        c: "", // Last price
-        Q: "", // Last quantity
-        o: "", // Open price
-        h: "", // High price
-        l: "", // Low price
-        v: "", // Total traded volume
-        q: "", // Total traded base asset volume
-        O: 0, // Statistics open time
-        C: 0, // Statistics close time
-        F: 0, // Last trade Id
-        n: 0, // Total number of trades
-      });
-    
-    const [action, setAction] = useState<string>("Buy");
-    const [quantity, setQuantity] = useState(0);
+  const [show, setShow] = useState<boolean>(false);
 
-    const [maxQuantity, setMaxQuantity] = useState(0);
-  
-    const [sellQuantity, setSellQuantity] = useState(0);
-  
-    const [maxSellQuantity, setMaxSellQuantity] = useState(0);
-      
-    let indexOfName = symbol?.search("USDT");
+  const [data, setData] = useState<TickerInterface>({
+    e: "", // Event type
+    E: 0, // Event time
+    s: "", // Symbol
+    ps: "", // Pair
+    p: "", // Price change
+    P: "", // Price change percent
+    w: "", // Weighted average price
+    c: "", // Last price
+    Q: "", // Last quantity
+    o: "", // Open price
+    h: "", // High price
+    l: "", // Low price
+    v: "", // Total traded volume
+    q: "", // Total traded base asset volume
+    O: 0, // Statistics open time
+    C: 0, // Statistics close time
+    F: 0, // Last trade Id
+    n: 0, // Total number of trades
+  });
 
-    let imageName = symbol?.slice(0, indexOfName).toLowerCase();
-  
-    let imageLink = import.meta.env.VITE_API_URL + "/icon/" + imageName;
-  
-    let lower = symbol.toLowerCase();
+  const [action, setAction] = useState<string>("Buy");
+  const [quantity, setQuantity] = useState(0);
 
-    let modalBody;
+  const [maxQuantity, setMaxQuantity] = useState(0);
 
-    const handleClose = () => setShow(false);
+  const [sellQuantity, setSellQuantity] = useState(0);
 
-    const handleShow = () => setShow(true);
+  const [maxSellQuantity, setMaxSellQuantity] = useState(0);
 
-    const changeQuantity = (event: any) => {
-      setQuantity(parseFloat(event.target.value));
-    };
-  
-    const changeSellQuantity = (event: any) => {
-      setSellQuantity(parseFloat(event.target.value));
-    };
-    
+  let indexOfName = symbol?.search("USDT");
+
+  let imageName = symbol?.slice(0, indexOfName).toLowerCase();
+
+  let imageLink = import.meta.env.VITE_API_URL + "/icon/" + imageName;
+
+  let lower = symbol.toLowerCase();
+
+  let modalBody;
+
+  const handleClose = () => setShow(false);
+
+  const handleShow = () => setShow(true);
+
+  const changeQuantity = (event: any) => {
+    setQuantity(parseFloat(event.target.value));
+  };
+
+  const changeSellQuantity = (event: any) => {
+    setSellQuantity(parseFloat(event.target.value));
+  };
+
   const handleForm = (e: any) => {
     e.preventDefault();
     e.stopPropagation();
 
     BuyCrypto(symbol, quantity, setCurrentBalance);
-    setShow(false)
+    setShow(false);
   };
 
   const handleSell = (e: any) => {
@@ -102,149 +100,146 @@ export default function SideCryptoPrice({symbol}: any) {
 
     SellCrypto(symbol, sellQuantity, setCurrentBalance);
   };
-  
-    const { lastJsonMessage  } = useWebSocket<any>(
-      `wss://fstream.binance.com/ws/${lower}@ticker`
-    );
-    
-    useEffect(() => {
-      if (lastJsonMessage !== null) {
-        setData(lastJsonMessage);
-      }
-    }, [lastJsonMessage]);
 
-    useEffect(() => {
-      if (auth == true) {
-        modalBody = (
-          <Tabs
-            defaultActiveKey="buy"
-            id="uncontrolled-tab-example"
-            onSelect={(e) => {
-              if (e == "buy") {
-                setAction("Buy");
-              } else if (e == "sell") {
-                setAction("Sell");
-              }
+  const { lastJsonMessage } = useWebSocket<any>(
+    `wss://fstream.binance.com/ws/${lower}@ticker`
+  );
+
+  useEffect(() => {
+    if (lastJsonMessage !== null) {
+      setData(lastJsonMessage);
+    }
+  }, [lastJsonMessage]);
+
+  if (auth == true) {
+    modalBody = (
+      <Tabs
+        defaultActiveKey="buy"
+        id="uncontrolled-tab-example"
+        onSelect={(e) => {
+          if (e == "buy") {
+            setAction("Buy");
+          } else if (e == "sell") {
+            setAction("Sell");
+          }
+        }}
+      >
+        <Tab eventKey="buy" title="Buy">
+          <input
+            type="range"
+            max={maxQuantity}
+            min={0}
+            value={quantity}
+            onChange={changeQuantity}
+            step={0.1}
+          />
+
+          <input
+            type="number"
+            max={maxQuantity}
+            className={Class.input}
+            value={quantity}
+            onChange={changeQuantity}
+          ></input>
+
+          <button
+            type="button"
+            className={Class.button + " " + Class.buttonSubmit}
+            onClick={(e) => {
+              handleForm(e);
             }}
           >
-            <Tab eventKey="buy" title="Buy">
-              <input
-                type="range"
-                max={maxQuantity}
-                min={0}
-                value={quantity}
-                onChange={changeQuantity}
-                step={0.1}
-              />
-    
-              <input
-                type="number"
-                max={maxQuantity}
-                className={Class.input}
-                value={quantity}
-                onChange={changeQuantity}
-              ></input>
-    
-              <button
-                type="button"
-                className={Class.button + " " + Class.buttonSubmit}
-                onClick={(e) => {
-                  handleForm(e)
-                }}
-              >
-                Buy
-              </button>
-            </Tab>
-            <Tab eventKey="sell" title="Sell">
-              <input
-                type="range"
-                max={maxSellQuantity}
-                min={0}
-                value={sellQuantity}
-                onChange={changeSellQuantity}
-                step={0.1}
-              />
-    
-              <Form.Control
-                type="number"
-                max={maxSellQuantity}
-                className={Class.input}
-                value={sellQuantity}
-                onChange={changeSellQuantity}
-              ></Form.Control>
-    
-              <button
-                type="button"
-                className={Class.button + " " + Class.buttonSubmit}
-                onClick={(e) => {
-                  handleSell(e)
-                }}
-              >
-                Sell
-              </button>
-            </Tab>
-          </Tabs>
-        );
-      } else {
-        modalBody = (
-          <div className={Class.loginHrefCont}>
-            <Link to="/login" className={Class.loginHref}>
-              Login
-            </Link>
-          </div>
-        );
-      }
-    })
+            Buy
+          </button>
+        </Tab>
+        <Tab eventKey="sell" title="Sell">
+          <input
+            type="range"
+            max={maxSellQuantity}
+            min={0}
+            value={sellQuantity}
+            onChange={changeSellQuantity}
+            step={0.1}
+          />
 
-    return(
-        <div>
-        <table className={Class.table}>
-          <tbody>
-            <tr>
-              <td>
-                <img src={imageLink} alt={symbol + "icon"}></img> {symbol}
-              </td>
-            </tr>
-            <tr>
-              <td>Open price:</td>
-              <td>{parseFloat(data.o)}</td>
-            </tr>
+          <Form.Control
+            type="number"
+            max={maxSellQuantity}
+            className={Class.input}
+            value={sellQuantity}
+            onChange={changeSellQuantity}
+          ></Form.Control>
 
-            <tr>
-              <td>Low price:</td>
-              <td>{parseFloat(data.l)}</td>
-            </tr>
+          <button
+            type="button"
+            className={Class.button + " " + Class.buttonSubmit}
+            onClick={(e) => {
+              handleSell(e);
+            }}
+          >
+            Sell
+          </button>
+        </Tab>
+      </Tabs>
+    );
+  } else {
+    modalBody = (
+      <div className={Class.loginHrefCont}>
+        <Link to="/login" className={Class.loginHref}>
+          Login
+        </Link>
+      </div>
+    );
+  }
 
-            <tr>
-              <td>High price:</td>
-              <td>{parseFloat(data.h)}</td>
-            </tr>
+  return (
+    <div>
+      <table className={Class.table}>
+        <tbody>
+          <tr>
+            <td>
+              <img src={imageLink} alt={symbol + "icon"}></img> {symbol}
+            </td>
+          </tr>
+          <tr>
+            <td>Open price:</td>
+            <td>{parseFloat(data.o)}</td>
+          </tr>
 
-            <tr>
-              <td>Current price:</td>
-              <td>{parseFloat(data.c)}</td>
-            </tr>
+          <tr>
+            <td>Low price:</td>
+            <td>{parseFloat(data.l)}</td>
+          </tr>
 
-            <tr>
-              <td>Price change:</td>
-              <td>
-                {parseFloat(data.p)}$ {parseFloat(data.P).toFixed(2)}%
-              </td>
-            </tr>
+          <tr>
+            <td>High price:</td>
+            <td>{parseFloat(data.h)}</td>
+          </tr>
 
-            <tr>
-              <td>Volume:</td>
-              <td>{parseFloat(data.q).toFixed(2)}</td>
-            </tr>
-          </tbody>
-        </table>
+          <tr>
+            <td>Current price:</td>
+            <td>{parseFloat(data.c)}</td>
+          </tr>
 
-        <Button onClick={handleShow} className={Class.button}>
-          Buy/Sell {symbol}
-        </Button>
+          <tr>
+            <td>Price change:</td>
+            <td>
+              {parseFloat(data.p)}$ {parseFloat(data.P).toFixed(2)}%
+            </td>
+          </tr>
 
+          <tr>
+            <td>Volume:</td>
+            <td>{parseFloat(data.q).toFixed(2)}</td>
+          </tr>
+        </tbody>
+      </table>
 
-        <Modal
+      <Button onClick={handleShow} className={Class.button}>
+        Buy/Sell {symbol}
+      </Button>
+
+      <Modal
         show={show}
         onHide={handleClose}
         backdrop="static"
@@ -267,6 +262,6 @@ export default function SideCryptoPrice({symbol}: any) {
           </Button>
         </Modal.Footer>
       </Modal>
-        </div>
-    )
+    </div>
+  );
 }
